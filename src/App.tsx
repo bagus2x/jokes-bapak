30a4ed944cbecdcd8cd357b48db1a1489023f9be
Tubagus <dutapanggulu@gmail.com>
2021-08-07 10:06:39 +0700

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import style from './App.module.css';
+
+interface Joke {
+	id: string;
+	joke: string;
+}
+
+const getJoke = (): Promise<Joke> => {
+	return fetch('https://icanhazdadjoke.com/', {
+		headers: {
+			Accept: 'application/json'
+		}
+	}).then((res) => res.json());
+};
+
+const useDadJoke = () => {
+	const [joke, setJoke] = useState<Joke | null>(null);
+	const [error, setError] = useState<Error | null>(null);
+
+	const refetch = () => {
+		setError(null);
+		setJoke(null);
+		getJoke()
+			.then((res) => setJoke(res))
+			.catch((err) => setError(err));
+	};
+
+	useEffect(() => {
+		refetch();
+	}, []);
+
+	return {
+		joke,
+		error,
+		isLoading: !joke && !error,
+		isError: !!error,
+		isSuccess: !!joke,
+		refetch
+	};
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const { joke, error, isError, isLoading, isSuccess, refetch } = useDadJoke();
+
+	useEffect(() => {
+		if (isError) alert(error?.message);
+	}, [isError, error]);
+
+	return (
+		<div className={style.container}>
+			<div className={style.jokeWrapper}>
+				{isSuccess && <span>{joke?.joke}</span>}
+				{isLoading && <span>Loading...</span>}
+			</div>
+			<button disabled={isLoading} className={style.btnRefresh} onClick={refetch}>
+				Refresh
+			</button>
+		</div>
+	);
 }
 
 export default App;
